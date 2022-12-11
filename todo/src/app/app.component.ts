3454a7f7dc3373bd34a37cc38a076cb1711db4d6
innaya12 <yakubov.inna2@gmail.com>
2022-12-11 10:56:47 +0200

    first app structure

--- todo/src/app/app.component.ts
+++ todo/src/app/app.component.ts
@@ -5,6 +5,22 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'todo';
+  title = 'Todo';
+
+  filter: 'all' | 'active' | 'done' = 'all';
+
+  allItems = [
+    { description: 'run', done: true },
+    { description: 'yoga', done: false },
+  ];
+
+  get items() {
+    if (this.filter === 'all') {
+      return this.allItems;
+    }
+    return this.allItems.filter((item) => this.filter === 'done' ? item.done : !item.done);
+  }
+
 }
