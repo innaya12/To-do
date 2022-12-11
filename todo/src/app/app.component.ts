59d58bd42ecf4eabe57d99971e95c5f777dc48e8
innaya12 <yakubov.inna2@gmail.com>
2022-12-11 11:51:31 +0200

    list functionality works

--- todo/src/app/app.component.ts
+++ todo/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Item } from "./item";
 
 @Component({
   selector: 'app-root',
@@ -21,6 +22,17 @@ export class AppComponent {
       return this.allItems;
     }
     return this.allItems.filter((item) => this.filter === 'done' ? item.done : !item.done);
+  }
+
+  addItem(description: string) {
+    this.allItems.unshift({
+      description,
+      done: false
+    });
+  }
+  
+  remove(item: Item) {
+    this.allItems.splice(this.allItems.indexOf(item), 1);
   }
 
 }
